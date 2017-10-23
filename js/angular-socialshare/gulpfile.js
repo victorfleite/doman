90fe309e0df07ec582199da0b09c09f58dddf7ab
VictorLeite <victor.leite@gmail.com>
2017-10-23 15:59:20 -0200

    removendo pacotes inuteis

--- js/angular-socialshare/gulpfile.js
+++
@@ -1,6 +0,0 @@
-﻿/*global require*/
-(function gulpConfig(require) {
-  'use strict';
-
-  require('require-dir')('conf/tasks');
-}(require));
--- js/angular-socialshare/index.js
+++
@@ -1,7 +0,0 @@
-﻿(function commonJS(require, module) {
-    'use strict';
-
-    require('./lib/angular-socialshare');
-
-    module.exports = '720kb.socialshare';
-}(require, module));
