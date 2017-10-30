e2fa1026cfb666911b2a5d86b9bd94a3d32f2e14
VictorLeite <victor.leite@gmail.com>
2017-10-30 21:22:05 -0200

    modal para atividade-item

--- app/atividade/atividade.item.controller.js
+++ app/atividade/atividade.item.controller.js
@@ -2,13 +2,86 @@
 
     'use strict';
 
+    angular.module('app')
+        .controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+            $scope.items = items;
+
+            $scope.myInterval = 5000;
+            $scope.noWrapSlides = false;
+            $scope.active = 0;
+            var slides = $scope.slides = [];
+            var currIndex = 0;
+
+            $scope.addSlide = function () {
+                var newWidth = 800 + slides.length + 1;
+                slides.push({
+                    image: '//unsplash.it/' + newWidth + '/550',
+                    text: ['', '', '', ''][slides.length % 4],
+                    id: currIndex++
+                });
+            };
+
+            $scope.randomize = function () {
+                var indexes = generateIndexesArray();
+                assignNewIndexesToSlides(indexes);
+            };
+
+            for (var i = 0; i < 4; i++) {
+                $scope.addSlide();
+            }
+
+            // Randomize logic below
+
+            function assignNewIndexesToSlides(indexes) {
+                for (var i = 0, l = slides.length; i < l; i++) {
+                    slides[i].id = indexes.pop();
+                }
+            }
+
+            function generateIndexesArray() {
+                var indexes = [];
+                for (var i = 0; i < currIndex; ++i) {
+                    indexes[i] = i;
+                }
+                return shuffle(indexes);
+            }
+
+            // http://stackoverflow.com/questions/962802#962890
+            function shuffle(array) {
+                var tmp, current, top = array.length;
+
+                if (top) {
+                    while (--top) {
+                        current = Math.floor(Math.random() * (top + 1));
+                        tmp = array[current];
+                        array[current] = array[top];
+                        array[top] = tmp;
+                    }
+                }
+
+                return array;
+            }
+
+            $scope.selected = {
+                item: $scope.items[0]
+            };
+
+            $scope.ok = function () {
+                $uibModalInstance.close($scope.selected.item);
+            };
+
+            $scope.cancel = function () {
+                $uibModalInstance.dismiss('cancel');
+            };
+        });
+
     angular
         .module('app')
         .controller('AtividadeItemController', atividadeItemController);
 
-    atividadeItemController.$inject = ['$scope', 'authService'];
+    atividadeItemController.$inject = ['$scope', 'authService', '$uibModal', '$document'];
 
-    function atividadeItemController($scope, authService) {
+    function atividadeItemController($scope, authService, $uibModal, $document) {
 
         // GRAFICO PIZZA
         $scope.percent = 65;
@@ -28,12 +101,46 @@
         $scope.rate = 5;
         $scope.max = 5;
         $scope.isReadonly = false;
-      
-        $scope.hoveringOver = function(value) {
-          $scope.overStar = value;
-          //$scope.percentRating = 100 * (value / $scope.max);
+
+        $scope.hoveringOver = function (value) {
+            $scope.overStar = value;
+            //$scope.percentRating = 100 * (value / $scope.max);
+        };
+
+        // MODAL ATIVIDADES
+
+        $scope.items = ['item1', 'item2', 'item3'];
+
+        $scope.open = function (size, parentSelector) {
+
+            var parentElem = parentSelector ?
+                angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+            var modalInstance = $uibModal.open({
+                animation: false,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$scope',
+                size: size,
+                appendTo: parentElem,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         };
 
     }
+
+
+
 
 })();
