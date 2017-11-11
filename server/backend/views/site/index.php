cda015baef0f8e0dec2109ea81b4d75d57be6b32
VictorLeite <victor.leite@gmail.com>
2017-11-11 17:18:08 -0200

    ajustes no dashboard

--- server/backend/views/site/index.php
+++ server/backend/views/site/index.php
@@ -91,32 +91,32 @@ use yii\helpers\Url;
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <div class="panel panel-primary">
+                <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-xs-3">
-                                <i class="fa fa-picture-o fa-5x"></i>
+                                <i class="fa fa-user-circle-o fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
                                 <div class="huge">
                                     <?php
-                                    echo \app\modules\doman\models\Atividade::find()->where(['deletado' => false])->count();
+                                    echo \app\modules\doman\models\User::find()->count();
                                     ?>
                                 </div>
-                                <div>Atividades</div>
+                                <div>Usuários</div>
                             </div>
                         </div>
                     </div>
-                    <a href="<?php echo Url::to(['/doman/atividade/index']); ?>">
+                    <a href="<?php echo Url::to(['/user']); ?>">
                         <div class="panel-footer">
-                            <span class="pull-left">Acessar Atividades</span>
+                            <span class="pull-left">Acessar Usuários do Sistema</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
 
                             <div class="clearfix"></div>
                         </div>
                     </a>
                 </div>
-            </div>
+            </div>            
         </div>
         <div class="row">
                 <div class="col-lg-3 col-md-6">
@@ -200,26 +200,50 @@ use yii\helpers\Url;
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
+             <div class="col-lg-3 col-md-6">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-xs-3">
-                                <i class="fa fa-user-circle-o fa-5x"></i>
+                                <i class="fa fa-cogs fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class="huge">
-                                    <?php
-                                    echo \app\modules\doman\models\User::find()->count();
-                                    ?>
+                                <div class="huge">                                   
                                 </div>
-                                <div>Usuários</div>
+                                <div></div>
                             </div>
                         </div>
                     </div>
-                    <a href="<?php echo Url::to(['/user']); ?>">
+                    <a href="<?php echo Url::to(['/admin']); ?>">
                         <div class="panel-footer">
-                            <span class="pull-left">Acessar Usuários do Sistema</span>
+                            <span class="pull-left">Controle de Acesso</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>       
+            
+        </div>
+        <div class="row">            
+            <div class="col-lg-3 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-picture-o fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">                                    
+                                </div>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="<?php echo Url::to(['/doman/atividade/index']); ?>">
+                        <div class="panel-footer">
+                            <span class="pull-left">Acessar Atividades</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
 
                             <div class="clearfix"></div>
