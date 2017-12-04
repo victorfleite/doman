e7829ae3205f39cfce54fc77b48f29fdb9e82298
VictorLeite <victor.leite@gmail.com>
2017-12-03 23:26:38 -0200

    melhorando pdf

--- server/backend/modules/doman/views/cartao/_reportView.php
+++ server/backend/modules/doman/views/cartao/_reportView.php
@@ -1,6 +1,17 @@
 
-<h1><?php echo $atividade->titulo ?></h1>
-<br>
+
+<table width="100%">
+    <tbody>
+        <tr>
+            <td style="margin-right: 10px;" width="20%">
+                <img src="valle.png" width="140px" height="75px">
+            </td>
+            <td style="text-align:left" width="80%" class="line-header">
+                <h1><?php echo $atividade->titulo ?></h1>
+            </td>
+        </tr>        
+    </tbody>
+</table>
 <table>
     <tbody>
         <tr>
@@ -18,7 +29,7 @@
                             <th class="text-right">Código Agrupador: </th><td class="text-right"><?php echo $agrupador ?></td>
                         </tr>
                     </tbody>
-                </table>
+                </table>               
 
             </td>
         </tr>        
@@ -38,46 +49,72 @@
         </tbody>
     </table>
 <?php } ?>
+<div style="text-align: center; margin-right: 20px">www.cvf-brasil.com</div>
 
-<pagebreak>
+<pagebreak page-break-type="clonebycss" />
+
+<p>&nbsp;</p>
+<div style="text-align: right;">verso</div>
+
+<table border="1" class="table-header-cartao">
+    <tbody>
+        <tr>
+            <th class="nome-cartao">
+                <?php echo $cartao->nome ?>
+            </th>
+        </tr>
+        <tr>
+            <td class="agrupador">
+                agrupador: <?php echo $agrupador ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<p>&nbsp;</p>
+<div style="text-align: center;"><img src="valle.png"></div>
+<div style="text-align: center;">www.cvf-brasil.com</div>
+
+<?php
+$cartoes = $atividade->getCartoes()->where(["deletado" => false, "status" => 1])->all();
+$i = 0;
+foreach ($cartoes as $cartao) {
+    ?>
+    <pagebreak page-break-type="clonebycss" />
+
+    <table class="table-imagem">
+        <tbody>
+            <tr>
+                <td style="text-align: center"><img src="<?php echo $cartao->imagem_caminho ?>" width="100%"></td>
+            </tr>
+        </tbody>
+    </table>
+
+    <pagebreak page-break-type="clonebycss" />
+
+    <p>&nbsp;</p>
+    <div style="text-align: right;">verso</div>
+
+    <table border="1" class="table-header-cartao">
+        <tbody>
+            <tr>
+                <th class="nome-cartao">
+                    <?php echo $cartao->nome ?>
+                </th>
+            </tr>
+            <tr>
+                <td class="agrupador">
+                    agrupador: <?php echo $agrupador ?>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p>&nbsp;</p>
+    <div style="text-align: center;"><img src="valle.png"></div>
+    <div style="text-align: center;">www.cvf-brasil.com</div>
+
+
 
     <?php
-    $cartoes = $atividade->getCartoes()->where(["deletado" => false, "status" => 1])->all();
-    $i = 0;
-    foreach ($cartoes as $cartao) {
-        ?>
-    
-        <table class="table-imagem">
-            <tbody>
-                <tr>
-                    <td style="text-align: center"><img src="<?php echo $cartao->imagem_caminho ?>" width="100%"></td>
-                </tr>
-            </tbody>
-        </table>
-
-        <pagebreak>
-            <div width="100%" class="verso">verso</div>
-            <table border="1" class="table-header-cartao">
-                <tbody>
-                    <tr>
-                        <th class="nome-cartao">
-                            <?php echo $cartao->nome?>
-                        </th>
-                    </tr>
-                    <tr>
-                        <td class="agrupador">
-                            Agrupador: <?php echo $agrupador ?>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <?php if (($i + 1) != count($cartoes)) { ?>
-
-                <pagebreak>
-
-                <?php } ?>
-                <?php
-                $i++;
-            }
-            ?>
+    $i++;
+}
+?>
